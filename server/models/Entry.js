41b430e0b2b21c023250e77d9e88bbde52afe9d8
andie <andrea_sue2@icloud.com>
2023-03-31 20:09:21 -0500

    create new entry model

---
+++ server/models/Entry.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EntrySchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  location: {
+    type: String,
+    enum: ['Point'],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+  text: {
+    type: String,
+    required: false,
+  },
+  pictures: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+  ratings: {
+    treeCover: { type: Number, min: 0, max: 5, required: false },
+    adaCompliance: { type: Number, min: 0, max: 5, required: false },
+    noise: { type: Number, min: 0, max: 5, required: false },
+    litter: { type: Number, min: 0, max: 5, required: false },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Entry = mongoose.model('Entry', EntrySchema);
